5bce520ec87782bec11cb1749aefd5e7b8265550
Martin van Vliet <mpvvliet@gmail.com>
2016-04-16 08:58:08 +0200

    Bestanden verplaatst

--- chat.py
+++
@@ -1,8 +0,0 @@
-import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-
-chatMsg = raw_input("Enter a message: ")
-
-while chatMsg != "/exit":
-    mc.postToChat(chatMsg)
-    chatMsg = raw_input("Enter a message: ")
--- teleport.py
+++
@@ -1,8 +0,0 @@
-import mcpi.minecraft as minecraft
-mc = minecraft.Minecraft.create()
-
-x=10
-y=11
-z=12
-
-mc.player.setPos(x, y, z)
